0fc105400ab4d1ffe7c5d5cc8c1ef2ba03352ffd
Einar Persson <einar.jpg@gmail.com>
2022-05-12 23:56:13 +0200

    add additional cmfs and illuminants

--- src/spectral-data/color-matching-functions.ts
+++ src/spectral-data/color-matching-functions.ts
@@ -1,8 +1,22 @@
 import { MultiSpectralDistribution } from "../spectral-distribution/spectral-distribution";
+import cmf10deg06Data from "./data/color-matching-functions/cie-xyz-cmf-10deg-2006.json";
 import cmf2deg31Data from "./data/color-matching-functions/cie-xyz-cmf-2deg-1931.json";
+import cmf2deg06Data from "./data/color-matching-functions/cie-xyz-cmf-2deg-2006.json";
 
 export const CIE_1931_2DEG = new MultiSpectralDistribution(
   cmf2deg31Data.span as [number, number],
   cmf2deg31Data.interval,
   cmf2deg31Data.values
+);
+
+export const CIE_2006_2DEG = new MultiSpectralDistribution(
+  cmf2deg06Data.span as [number, number],
+  cmf2deg06Data.interval,
+  cmf2deg06Data.values
+);
+
+export const CIE_2006_10DEG = new MultiSpectralDistribution(
+  cmf10deg06Data.span as [number, number],
+  cmf10deg06Data.interval,
+  cmf10deg06Data.values
 );
--- src/spectral-data/illuminants.ts
+++ src/spectral-data/illuminants.ts
@@ -1,8 +1,20 @@
 import { SpectralDistribution } from "../spectral-distribution/spectral-distribution";
+import D50Data from "./data/illuminants/D50.json";
 import D65Data from "./data/illuminants/D65.json";
+import EData from "./data/illuminants/E.json";
 
 export const D65 = new SpectralDistribution(
   D65Data.span as [number, number],
   D65Data.interval,
   D65Data.values
+);
+export const D50 = new SpectralDistribution(
+  D50Data.span as [number, number],
+  D50Data.interval,
+  D50Data.values
+);
+export const E = new SpectralDistribution(
+  EData.span as [number, number],
+  EData.interval,
+  EData.values
 );
