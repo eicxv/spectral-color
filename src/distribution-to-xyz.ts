e1c48ece515324542dcb10872140f9f0fe677eae
Einar Persson <einar.jpg@gmail.com>
2022-05-17 23:44:09 +0200

    remove obsolete function

--- src/distribution-to-xyz.ts
+++ src/distribution-to-xyz.ts
@@ -2,10 +2,8 @@ import { CIE_1931_2DEG as cmf31 } from "./spectral-data/color-matching-functions
 import { D65 } from "./spectral-data/illuminants";
 import { SpectralDistribution } from "./spectral-distribution/spectral-distribution";
 
-type Vec3 = [number, number, number];
-
-function scaleVec(v: number[], s: number): number[] {
-  return v.map((x) => x * s);
+function scaleVec<T extends number[]>(v: T, s: number): T {
+  return v.map((x) => x * s) as T;
 }
 
 export function spdToXyz(
@@ -13,19 +11,12 @@ export function spdToXyz(
   cmfs: SpectralDistribution<[number, number, number]> = cmf31,
   emissive = false,
   illuminant: SpectralDistribution<number> = D65
-): Vec3 {
+): [number, number, number] {
   let xyz = cmfs.multiply(spec).sum();
   xyz = scaleVec(xyz, cmfs.shape.interval);
   if (!emissive) {
     const k = illuminant.combine(cmfs, (a, b) => a * b[1]).sum() * illuminant.shape.interval;
     xyz = scaleVec(xyz, 1 / k);
   }
-  return xyz as Vec3;
-}
-
-export function illuminateDistribution(
-  spec: SpectralDistribution<number>,
-  illuminant: SpectralDistribution<number> = D65
-): SpectralDistribution<number> {
-  return spec.combine(illuminant, (a, b) => a * b);
+  return xyz as [number, number, number];
 }
